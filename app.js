033d3777f84599ac649b7e6a95f631f2d8a26eb3
wuixwui <seoweet21@gmail.com>
2021-07-30 13:08:35 +0900

    feat: use classList for append and delete class
    using classList is not a change everything, only append and delete class
    name if your element contain class name to the condition

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ console.dir(title)
 function handleTitleClick() {
     // 변수 선언은 코드를 깔끔하게 해주고 오타로 인해 생기는 오류를 줄여 줄 수 있다.
     const clickedClass = "clicked"
-    if (title.className === clickedClass){
-        title.className = "";
-        //h1 에 class가 존재할 경우 해당 클래스를 무시 및 삭제 -> 지정한 클래스를 넣어버린다.
+    //classList 함수를 사용하면 모든 class들이 변경되는 것을 방지할 수 있다.
+        // class 중에 ()안에 들어간 class가 있는 지 확인한다.
+    if (title.classList.contains(clickedClass)) {
+        // 해당 class 삭제
+        title.classList.remove(clickedClass);
     } else {
-        title.className = clickedClass;
+        // 해당 class 추가
+        title.classList.add(clickedClass);
     }
 };
 
